c40a254d9c990e4432a31a812da55abce397acb3
Ian Geckeler <ikcgeckeler@gmail.com>
2018-06-06 18:06:57 -0700

    added call to action popup

---
+++ script.js
@@ -0,0 +1,22 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+
+
+$(document).ready(function() {
+    console.log('ready')
+    });
+    
+
+$('#myModal').modal({ show: false})
+
+setTimeout(function() {
+    $('#myModal').modal('show');  
+},5000)
+        
+    
+    
+    
+
+
+
